d12026d217b31c95d9d2a6d8df9b3742f079f89c
ThPacifist <47048369+ThPacifist@users.noreply.github.com>
2019-12-07 15:22:53 -0600

    Add files via upload

---
+++ Blademaster.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterHunterCompanion
+{
+    public class Blademaster : Weapon
+    {
+        public static int[] _sharpness;
+
+        // Work on specifics of special attack
+        public static int _specialAttack;
+
+        int x;
+        public override string _rarity
+        {
+            get { return "Rare" + x; }
+            set
+            {
+                if (Int32.Parse(value) <= 10 && Int32.Parse(value) >= 1)
+                    x = Int32.Parse(value);
+            }
+        }
+
+        public override int _slots
+        {
+            get { return x; }
+            set
+            {
+                if (value <= 3 && value >= 1)
+                    x = value;
+                else
+                    x = 0;
+            }
+        }
+    }
+}
